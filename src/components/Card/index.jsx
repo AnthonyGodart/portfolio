8c896ec3eac8a3ef9d774e613e5a6f0a52877f01
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-07-06 13:09:49 +0200

    adding responsive features

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -76,7 +76,7 @@ function MyCard() {
             key={projetKey}
             onClick={() => handleClick(projetKey)}
           >
-            <Card responsive className={isActive ? 'activeCard' : ''}>
+            <Card className={isActive ? styles.activeCard : ''}>
               {!isActive && (
                 <Card.Header className={styles.header}r>
                   <Col>
