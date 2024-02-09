b0c82090e3195dccfa648612467699a9c8433c00
Anthony GODART <anthonygodart@MacBook-Pro-de-Anthony.local>
2024-02-09 09:22:41 +0100

    implemented dynamic year in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,14 @@ import styles from './footer.module.css';
 import { NavLink } from 'react-router-dom';
 
 function Footer(){
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
     return (
         <footer className={styles.footer}>
         <NavLink className={styles.link} to="/">Accueil</NavLink>
-        <p className={styles.paragraph}> Site réalisé par Anthony GODART. © 2023 - Tous droits réservés. </p>
+        <p className={styles.paragraph}> Site réalisé par Anthony GODART. © {currentYear} - Tous droits réservés. </p>
     </footer>
     );
 }
